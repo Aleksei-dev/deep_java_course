cbe2b7739a6c34ea828dc178de02244c7fbd942d
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2021-04-03 07:27:50 +0300

    If else constructions created
    Checking values for Modification tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -3,44 +3,51 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by coval on 3/25/2021.
  */
 public class HelperBase {
-    protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-    public HelperBase(FirefoxDriver wd) {
-        this.wd = wd;
-    }
+  public HelperBase(WebDriver wd) {
+    this.wd = wd;
+  }
 
-    protected void click(By locator) {
-        wd.findElement(locator).click();
-    }
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        click(locator);
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      // if text not euqals to existingText
+      if (!text.equals(existingText)){
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
+      }
     }
+  }
 
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
 
 }
